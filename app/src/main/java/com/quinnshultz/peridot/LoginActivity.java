80c9eff4be48cd1b3860d5fa67d680b2e1a6c431
Quinn Shultz <vanoxite@gmail.com>
2019-03-14 20:33:07 -0600

    Added login activity layout

--- app/src/main/java/com/quinnshultz/peridot/LoginActivity.java
+++ app/src/main/java/com/quinnshultz/peridot/LoginActivity.java
@@ -82,7 +82,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.server_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {

f379cffe3356cc7cc998776d5885a82261c0720e
Quinn Shultz <vanoxite@gmail.com>
2019-03-14 21:08:51 -0600

    Added main screen layout

--- app/src/main/java/com/quinnshultz/peridot/LoginActivity.java
+++ app/src/main/java/com/quinnshultz/peridot/LoginActivity.java
@@ -3,6 +3,7 @@ package com.quinnshultz.peridot;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -333,6 +334,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Intent loginMain = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(loginMain);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
